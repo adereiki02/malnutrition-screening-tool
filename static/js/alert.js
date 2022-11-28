bdaeed90d951c236178d6d1312f63d0c082ba87f
adereiki02 <adereiki02@gmail.com>
2022-11-28 22:03:22 +0700

    Mengubah tombol 'Hitung IMT?'

--- static/js/alert.js
+++ static/js/alert.js
@@ -1,15 +1,17 @@
 function submitForm(form) {
-    swal ({
-      title: "Apakah Anda yakin?",
-      text: "Data tidak dapat diubah setelah dikirim",
-      icon: "warning",
-      buttons: true,
-      dangerMode: true,
-    })
-    .then((isOkay) => {
-      if(isOkay) {
-        form.submit();
-      }
-    });
-    return false;
-  } 
+  swal ({
+    title: "Apakah Anda yakin?",
+    text: "Data tidak dapat diubah setelah dikirim",
+    icon: "warning",
+    buttons: {
+      cancel: "Batal",
+    Ok: true,
+    },
+  })
+  .then((isOkay) => {
+    if(isOkay) {
+      form.submit();
+    }
+  });
+  return false;
+}
